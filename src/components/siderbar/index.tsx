b9c84269ba2bbf056446ad9a99b428b203c48437
Islombek <eislombek9599@gmail.com>
2024-04-01 01:50:20 +0900

    Fix: modify Sidebar with motion-frame

--- src/components/siderbar/index.tsx
+++ src/components/siderbar/index.tsx
@@ -4,29 +4,29 @@ import { FaGithub } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { ToggleButton } from "./toggleButton";
 import { Links } from "./links";
-
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      delay: 0.05,
+      type: "spring",
+      stiffness: 10,
+      damping: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
 export const Sidebar = () => {
   const [open, setOpen] = useState(false);
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 20,
-        damping: 40,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
+
   return (
     <motion.div className="sidebar" animate={open ? "open" : "closed"}>
       <motion.div className="bg" variants={variants}>
